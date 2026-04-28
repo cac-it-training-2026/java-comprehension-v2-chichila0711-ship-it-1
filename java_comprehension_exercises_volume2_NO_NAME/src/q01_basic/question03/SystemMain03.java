2c48cdd308c23486303ae9616ea9332f937b1697
tsuzurao <tsuzurao@C010423066.zipangu.cac.co.jp>
2026-04-28 15:53:13 +0900

    q01-question03完了

--- java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question03/SystemMain03.java
@@ -4,6 +4,14 @@ public class SystemMain03 {
 
 	public static void main(String[] args) {
 		//TODO ここから実装する
+		System.out.println("---SHOW DATA(NULL)--- ");
+
+		Member member1 = new Member();
+		Member member2 = new Member("Miura Manabu", 30, 2);
+
+		member1.showMember();
+		System.out.println("---SHOW DATA(Miura)--- ");
+		member2.showMember();
 
 	}
 
