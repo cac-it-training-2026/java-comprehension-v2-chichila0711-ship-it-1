f43e81e095cea37f7264c77268207e81a0c508f3
tsuzurao <tsuzurao@C010423066.zipangu.cac.co.jp>
2026-04-28 15:54:38 +0900

    q-01question05完了

---
+++ java_comprehension_exercises_volume2_NO_NAME/src/q01_basic/question05/MemberManager.java
@@ -0,0 +1,17 @@
+package q01_basic.question05;
+
+public class MemberManager {
+
+	private MemberManager() {
+	}
+
+	public static void showAllMembers(AbstMember[] members) {
+
+		for (AbstMember am : members) {
+			am.showMember();
+
+		}
+
+	}
+
+}
